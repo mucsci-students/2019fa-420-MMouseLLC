022a6138cc8f9eba8dc2a0ac08972d574363b20f
Matt Fossett <mattfoss01@gmail.com>
2019-09-15 18:05:44 -0400

    Added add method with unique checking

--- src/main/java/AddClass.java
+++ src/main/java/AddClass.java
@@ -1,17 +1,42 @@
 import java.util.List;
+import java.util.Scanner;
 
 
 public class AddClass {
 
 	public static void main(String[] args) {
-		UMLItem uml = new UMLItem(0, null, 0, null);
-		
 		UMLEnvironment uE = new UMLEnvironment();
-		
+		// REPL
+		// Only takes command add name
+		Scanner in = new Scanner (System.in);
+		while (in.hasNext()){
+			String cmdline = in.nextLine();
+			String[] cmd = cmdline.split(" ");
+			if (cmd[0].equals("add")){
+				if (!addClass(uE, 0, cmd[1], null)){
+					System.out.println(cmd[1] + " is already added.");
+				}
+			} else {
+				System.out.println("Invalid command");
+				continue;
+			}
+			// PRINT
+			System.out.print("[ ");
+			for (UMLItem i : uE.getItems()){
+				System.out.print(i.getName() + " ");
+			}
+			System.out.print("]\n");	
+		}
+		in.close();
 	}
 	
+	/**
+	 * Checks UMLEnvironment to see if any UMLItem's have name
+	 * @param umlEnv
+	 * @param name
+	 * @return true if name is unique to the provided environment
+	 */
 	public static boolean canAddUnique(UMLEnvironment umlEnv, String name){
-		
 		for (UMLItem i : umlEnv.Items){
 			if (i.getName().equals(name)){
 				return false;
@@ -21,12 +46,13 @@ public class AddClass {
 	}
 	
 	/**
+	 * Adds a new UMLItem object to a provided UMLEnvironment object
 	 * 
 	 * @param umlEnv
 	 * @param id
 	 * @param name
 	 * @param attr
-	 * @return
+	 * @return true if name is unique to the provided UMLEnvironment
 	 */
 	public static boolean addClass(UMLEnvironment umlEnv, int id, String name, List<String> attr){
 		if (canAddUnique(umlEnv, name)){
