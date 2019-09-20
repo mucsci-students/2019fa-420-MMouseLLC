5735b85f29bd9ef2203d932451ecc4ce9f1540f5
Lauren Deaver <laurendeaver@admins-MacBook-Air.local>
2019-09-20 10:51:41 -0400

    fixed save and load to work, added I/O exceptions to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Run();
 	}
 	
-	public static void Run() {
+	public static void Run() throws IOException  {
 		UMLEnvironment env = new UMLEnvironment();
 		Console.homeScreen(env);
 	}
