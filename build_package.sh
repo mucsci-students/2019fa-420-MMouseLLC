85ba639d9beabfd55fbb7a1f60ded748ae9e000d
djharten <djharten@millersville.edu>
2019-10-16 15:10:36 -0400

    fixed command line build issue, updated scripts and readme

--- build_package.sh
+++ build_package.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mvn clean
-mvn compile
+mvn clean install
 mvn package
-mvn assembly::single
-java -cp target/mmouse-uml-0.0.2-SNAPSHOT.jar:target/mmouse-uml-0.0.2-SNAPSHOT-jar-with-dependencies.jar utility.Main
+java -cp target/mmouse-uml-0.0.2-SNAPSHOT-jar-with-dependencies.jar utility.Main
